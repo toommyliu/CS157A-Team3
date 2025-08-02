cf55534cffcbe5f7aad79242ea1083c21d5342d6
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-08-02 15:26:51 -0700

    Add method to get medication logs by patient id

--- src/main/java/com/group_3/healthlink/services/MedicationLogService.java
+++ src/main/java/com/group_3/healthlink/services/MedicationLogService.java
@@ -1,11 +1,10 @@
 package com.group_3.healthlink.services;
 
 import com.group_3.healthlink.DatabaseMgr;
+import com.group_3.healthlink.MedicationLog;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class MedicationLogService {
   public static boolean createEntry(int medicationId, int patientId, String dosageTaken, String note, java.sql.Timestamp takenAt) {
@@ -38,6 +37,38 @@ public class MedicationLogService {
     } catch (java.sql.SQLException e) {
       System.out.println("Error creating new medication log: " + e.getMessage());
       return false;
+    }
+  }
+
+  public static MedicationLog[] getMedicationLogsByPatientId(int patientId) {
+    Connection con = DatabaseMgr.getInstance().getConnection();
+
+    String query = "SELECT * FROM medication_log WHERE patient_id = ?";
+
+    try {
+      PreparedStatement stmt = con.prepareStatement(query);
+      stmt.setInt(1, patientId);
+      java.sql.ResultSet rs = stmt.executeQuery();
+
+      java.util.List<MedicationLog> logs = new java.util.ArrayList<>();
+      while (rs.next()) {
+        MedicationLog log = new MedicationLog();
+        log.setMedicationLogId(rs.getInt("medication_log_id"));
+        log.setMedicationId(rs.getInt("medication_id"));
+        log.setPatientId(rs.getInt("patient_id"));
+        log.setTakenAt(rs.getTimestamp("taken_at"));
+        log.setDosageTaken(rs.getString("dosage_taken"));
+        log.setCreatedAt(rs.getTimestamp("created_at"));
+        log.setNote(rs.getString("note"));
+        logs.add(log);
+      }
+
+      rs.close();
+      stmt.close();
+      return logs.toArray(new MedicationLog[0]);
+    } catch (java.sql.SQLException e) {
+      System.out.println("Error get medication logs by patient id " + e.getMessage());
+      return new MedicationLog[0];
     }
   }
 }
